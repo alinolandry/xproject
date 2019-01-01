3c4c8c8716c7d144ae6841652a6082307a0293b7
Alain NOUTCHOMWO <alinolandry@gmail.com>
2019-01-01 20:59:43 +0100

    ajout des modesls dans l'administration

--- balengou/admin.py
+++ balengou/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from balengou.models import *
 # Register your models here.
+
+admin.site.register(Team)
+admin.site.register(Project)
+admin.site.register(ProductBacklog)
+admin.site.register(Sprint)
+admin.site.register(UserStory)
